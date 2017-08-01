8756ddd536f6fd12fb4c20c9304a5e62da420b3a
madanmk07 <30584127+madanmk07@users.noreply.github.com>
2017-08-01 22:59:32 +0530

    First change test sequence

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-System.out.println("Hello There!!");
+System.out.println("Hello There!! wassup");
 		// TODO Auto-generated method stub
 
 	}
